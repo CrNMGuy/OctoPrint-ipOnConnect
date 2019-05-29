f241a6b9a7f36d6c57080dd19ef9d3bb73cff4da
CrNMGuy <florian.duval@gmail.com>
2019-05-29 22:20:30 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,10 +11,10 @@ plugin_package = "octoprint_ipOnConnect"
 
 # The plugin's human readable name. Can be overwritten within OctoPrint's internal data via __plugin_name__ in the
 # plugin module
-plugin_name = "OctoPrint-ipOnConnect"
+plugin_name = "OctoPrint-ipOnConnect-Beep"
 
 # The plugin's version. Can be overwritten within OctoPrint's internal data via __plugin_version__ in the plugin module
-plugin_version = "0.2.2"
+plugin_version = "0.2.3"
 
 # The plugin's description. Can be overwritten within OctoPrint's internal data via __plugin_description__ in the plugin
 # module
